15b1141b3b5d4133c9908370abced0741980649b
OlkhovykBoris <brominecraft123@gmail.com>
2016-03-19 11:58:47 +0200

    different stuff

---
+++ C#/Hello wrld C3 first.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Hellowrld
+{
+	class Hello
+	{
+		static void Main()
+		{
+			Console.WritrLine("Helo world");
+
+			Console.WriteLine("Press any key to exist")
+			Console.ReaadKey();
+		}
+	}
+}
---
+++ C#/firstconsole/firstconsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hellowrld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            int a=10;
+            int b=1;
+            a += b;
+
+            Console.WriteLine(a);
+            
+            if(a>10)
+            {
+                Console.WriteLine("Hello wrld");
+
+                Console.WriteLine("Press any key to exist");
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
